411837c6a0bf549361d64305bf9c693c55a94b99
Shady <shady@shadyproject.net>
2014-12-01 23:15:32 +0700

    stubbed out methods for controller

--- ISB/Core/ImportController.swift
+++ ISB/Core/ImportController.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+enum ImportStatus {
+    case NotStarted
+    case Running (Float) //percent done (approximate)
+    case Succeeded (NSData) //raw data for audio track
+    case Failed (NSError) //error that occured
+}
+
+protocol ImportControllerDelegate {
+    func status(ImportStatus) -> Void
+    func didStart(ImportStatus) -> Void
+    func didFinish(ImportStatus) -> Void
+}
+
+class ImportController {
+
+    internal var delegate: ImportControllerDelegate!
+
+    init(delegate: ImportControllerDelegate) {
+        self.delegate = delegate
+    }
+
+    internal func startImport(fromUrl:NSURL!) {
+        println("Starting Import")
+        delegate.didStart((ImportStatus.Running(0.0)))
+
+        println("Halfway There")
+        delegate.status(ImportStatus.Running(50.0))
+
+        println("Almost Done")
+        delegate.status(ImportStatus.Running(99.9))
+
+        println("Finished")
+        delegate.status(ImportStatus.Succeeded("Finished".dataUsingEncoding(NSUTF8StringEncoding)!))
+    }
+}
