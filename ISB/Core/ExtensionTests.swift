9fbd09a91982d01062da68154452887a2a1b1452
Shady <shady@shadyproject.net>
2014-12-26 13:55:26 -0600

    tests and such

--- ISB/Core/ExtensionTests.swift
+++ ISB/Core/ExtensionTests.swift
@@ -10,8 +10,21 @@ import Foundation
 import XCTest
 
 class ExtensionTests: XCTestCase {
+    
     func testEmptyQuerySting() {
         let params = "http://www.youtube.com".dictionaryFromQueryStringComponents()
         XCTAssertEqual(params.count, 0, "There should be no entires in the dictionary")
+    }
+    
+    func testKeyNoValueFromString() {
+        let noParam = "http://www.youtube.com/?thing".dictionaryFromQueryStringComponents()
+        XCTAssertEqual(noParam.count, 0, "There should be no entires in the dictionary")
+    }
+    
+    func testEmptyParamFromString() {
+        let emptyParam = "http://www.youtube.com/?thing=".dictionaryFromQueryStringComponents()
+        XCTAssertEqual(emptyParam.count, 1, "The dict should have 1 entry")
+        let array = emptyParam["thing"]!
+        XCTAssertEqual(array.first!, "", "The member value should be the empty string")
     }
 }
