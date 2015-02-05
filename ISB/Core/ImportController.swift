8d7a49bd9266604f77ddefeebd85387e9a56235c
Shady <shady@shadyproject.net>
2015-02-05 14:36:06 -0800

    dont even remember what this does

--- ISB/Core/ImportController.swift
+++ ISB/Core/ImportController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import AVFoundation
 
 enum ImportStatus {
     case NotStarted
@@ -36,16 +37,45 @@ class ImportController {
         //TODO this should maybe happen on a background thread since we're doing network stuff
         println("Parsing URL")
         let info = YoutubeParser.h264VideosForYoutubeUrl(youtubeUrl: fromUrl)
-        println("Got Url")
         delegate.status(ImportStatus.Running(10.0))
         
         println("Loading stream into AVFoundation")
-        delegate.status(ImportStatus.Running(50.0))
+        let asset = AVAsset.assetWithURL(info.smallUrl!) as AVURLAsset
+        delegate.status(ImportStatus.Running(20.0))
+        
+        var loadingPercent = Float(20.0)
+        
+        asset.loadValuesAsynchronouslyForKeys(["tracks"], completionHandler: { () -> Void in
+            var error:NSError? = nil
+            let status = asset.statusOfValueForKey("tracks", error: &error)
+            
+            switch status {
+            case .Loaded:
+                loadingPercent += Float(10.0)
+                self.delegate.status(.Running(loadingPercent))
+                
+                for track in asset.tracks {
+                    if track.mediaType == AVMediaTypeAudio {
+                        track
+                    }
+                }
+                
+                
+                println("Almost Done")
+                self.delegate.status(ImportStatus.Running(99.9))
 
-        println("Almost Done")
-        delegate.status(ImportStatus.Running(99.9))
-
-        println("Finished")
-        delegate.status(ImportStatus.Succeeded("Finished".dataUsingEncoding(NSUTF8StringEncoding)!))
+                println("Finished")
+                self.delegate.status(ImportStatus.Succeeded("Finished".dataUsingEncoding(NSUTF8StringEncoding)!))
+            case .Failed:
+                self.delegate.status(.Failed(error!))
+            case .Cancelled:
+                self.delegate.didFinish(.Failed(NSError(domain: "Cancelled", code: 0, userInfo: nil)))
+            case .Unknown:
+                self.delegate.status(.Failed(NSError(domain: "Unknown", code: 1, userInfo: nil)))
+            case .Loading:
+                loadingPercent += 0.1
+                self.delegate.status(ImportStatus.Running(loadingPercent))
+            }
+        })
     }
 }
